842f6193975d449297e00e45a25d0c85f8296420
Fabio Nokai <fnokai@nic.br>
2017-08-16 10:18:05 -0400

    Corrigindo local do arquivo DataInvalidEx

--- src/main/java/br/com/caelum/agiletickets/models/DataInvalidaException.java
+++ src/main/java/br/com/caelum/agiletickets/models/DataInvalidaException.java
@@ -2,6 +2,11 @@ package br.com.caelum.agiletickets.models;
 
 public class DataInvalidaException extends RuntimeException {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
 	public DataInvalidaException(String message) {
 		super(message);
 		// TODO Auto-generated constructor stub
--- src/test/java/br/com/caelum/agiletickets/models/EspetaculoTest.java
+++ src/test/java/br/com/caelum/agiletickets/models/EspetaculoTest.java
@@ -1,6 +1,8 @@
 package br.com.caelum.agiletickets.models;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import java.util.Iterator;
 import java.util.List;
@@ -83,103 +85,96 @@ public class EspetaculoTest {
 		Sessao sessao = new Sessao();
 		sessao.setTotalIngressos(quantidade * 2);
 		sessao.setIngressosReservados(quantidade);
-	
+
 		return sessao;
 	}
-	
+
 	@Test
 	public void ValidarPeriodicidadeDiaria() {
 		Espetaculo ivete = new Espetaculo();
-		
+
 		LocalDate diaInicio = new LocalDate();
 		LocalDate diaFim = diaInicio.plusDays(3);
-		
+
 		LocalTime hora = new LocalTime();
-		
+
 		List<Sessao> listSessoes = ivete.criaSessoes(diaInicio, diaFim, hora, Periodicidade.DIARIA);
-		
+
 		assertEquals(4, listSessoes.size());
 	}
-	
+
 	@Test
 	public void ValidarPeriodicidadeSemanal() {
 		Espetaculo ivete = new Espetaculo();
-		
+
 		LocalDate diaInicio = new LocalDate();
 		LocalDate diaFim = diaInicio.plusDays(30);
-		
+
 		LocalTime hora = new LocalTime();
-		
+
 		List<Sessao> listSessoes = ivete.criaSessoes(diaInicio, diaFim, hora, Periodicidade.SEMANAL);
-		
+
 		assertEquals(5, listSessoes.size());
 	}
-	
+
 	@Test
 	public void ValidarSessaoCriadaDiaria() {
 		Espetaculo ivete = new Espetaculo();
 		LocalDate diaInicio = new LocalDate();
-		LocalDate diaFim =  new LocalDate();
+		LocalDate diaFim = new LocalDate();
 		LocalTime hora = new LocalTime();
-		
+
 		List<Sessao> listSessoes = ivete.criaSessoes(diaInicio, diaFim, hora, Periodicidade.DIARIA);
-		
+
 		Sessao sessao = listSessoes.get(0);
-		
+
 		String diaIni = diaInicio.toString(DateTimeFormat.shortDate().withLocale(new Locale("pt", "BR")));
 		String horaIni = hora.toString(DateTimeFormat.shortTime().withLocale(new Locale("pt", "BR")));
-		
+
 		assertEquals(sessao.getDia(), diaIni);
 		assertEquals(sessao.getHora(), horaIni);
 	}
-	
+
 	@Test
 	public void ValidarSessoesCriadasSemanal() {
 		Espetaculo ivete = new Espetaculo();
 		LocalDate diaInicio = new LocalDate();
-		LocalDate diaFim =  diaInicio.plusDays(30);
+		LocalDate diaFim = diaInicio.plusDays(30);
 		LocalTime hora = new LocalTime();
-		
+
 		List<Sessao> listSessoes = ivete.criaSessoes(diaInicio, diaFim, hora, Periodicidade.SEMANAL);
-		
+
 		Iterator<Sessao> it = listSessoes.iterator();
-		
+
 		String diaIni = diaInicio.toString(DateTimeFormat.shortDate().withLocale(new Locale("pt", "BR")));
 		String horaIni = hora.toString(DateTimeFormat.shortTime().withLocale(new Locale("pt", "BR")));
 
 		assertEquals(5, listSessoes.size());
-		
-		while(it.hasNext()) {
+
+		while (it.hasNext()) {
 			Sessao s = (Sessao) it.next();
 
-			assertEquals( diaIni, s.getDia());
+			assertEquals(diaIni, s.getDia());
 			assertEquals(horaIni, s.getHora());
-			
+
 			diaInicio = diaInicio.plusDays(7);
 			diaIni = diaInicio.toString(DateTimeFormat.shortDate().withLocale(new Locale("pt", "BR")));
 
 		}
-		
-		
+
 	}
-	
-	
-	
-	
-	
-	@Test (expected=DataInvalidaException.class)
+
+	@Test(expected = DataInvalidaException.class)
 	public void ValidarSessaoInvalida() {
 		Espetaculo ivete = new Espetaculo();
 
-		LocalDate diaFim =  new LocalDate();
+		LocalDate diaFim = new LocalDate();
 		LocalDate diaInicio = diaFim.plusDays(1);
-		
-		LocalTime hora = new LocalTime();
-		
-		List<Sessao> listSessoes = ivete.criaSessoes(diaInicio, diaFim, hora, Periodicidade.DIARIA);
 
-			
+		LocalTime hora = new LocalTime();
+
+		ivete.criaSessoes(diaInicio, diaFim, hora, Periodicidade.DIARIA);
+
 	}
-	
-	
+
 }
